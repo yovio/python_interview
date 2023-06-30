cdacd1dfc65efb7cf860ab8f4211d1bf3ec57e82
Yovi Oktofianus <yovio@hotmail.com>
2023-06-30 23:03:52 +0000

    Complete

--- interview/weather.py
+++ interview/weather.py
@@ -1,3 +1,77 @@
+from datetime import datetime, date
+
+
+class WeatherDataLine:
+    def __init__(self, line: str):
+        self._line_items = line.split(',')
+    
+    @property
+    def station(self):
+        return self._line_items[0]
+
+    @property
+    def timestamp(self):
+        return datetime.strptime(self._line_items[1], "%m/%d/%Y %I:%M:%S %p")
+
+    @property
+    def air_temp(self):
+        return float(self._line_items[2])
+
+    @property
+    def date(self):
+        return self.timestamp.date()
+
+    @property
+    def summary_key(self):
+        return f"{self.station}-{self.timestamp.date():%Y-%m-%d}"
+
+    def __repr__(self):
+        return f"WeatherDataLine({self.station}, {self.timestamp}, {self.air_temp})"
+        
+class SummaryWeatherData:
+    def __init__(self, data: WeatherDataLine):
+        self.station = data.station
+        self.date = data.date
+        self.min = data.air_temp
+        self.max = data.air_temp
+        self.first = data.air_temp
+        self.last = data.air_temp
+
+    def add_data(self, data: WeatherDataLine):
+        if self.min > data.air_temp:
+            self.min = data.air_temp
+        if self.max < data.air_temp:
+            self.max = data.air_temp
+
+        self.first = data.air_temp # We do this because the data shown that it was written in descending timeline. If data is unordered, we need to keep timestamp and compare it so we know 1st and last
+
+
+    def __str__(self):
+        return f"{self.station},{self.date:%m/%d/%Y},{self.min},{self.max},{self.first},{self.last}"
 
 def process_csv(reader, writer):
-    writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
+    summary_data = {}
+    line = reader.readline().strip()
+    while line:
+        line = reader.readline()        
+        if 'Station Name,Measurement Timestamp' in line: # last line contains this
+            break
+        
+        if line:
+            try:
+                wd = WeatherDataLine(line)
+                
+                cur_data = summary_data.get(wd.summary_key, None)        
+                if cur_data is None:
+                    cur_data = SummaryWeatherData(wd)
+                    summary_data[wd.summary_key] = cur_data
+                else:
+                    cur_data.add_data(wd)
+            except Exception as ex:
+                print(f"exception on line: {line} Ex: {ex}")
+                break
+
+    writer.write("Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp\n")
+    
+    for sum_val in summary_data.values():
+        writer.write(f"{sum_val}\n")
--- test/weather_test.py
+++ test/weather_test.py
@@ -1,9 +1,50 @@
 from interview import weather
 import io
+from datetime import datetime, date
+import pandas as pd
+import os
 
-
-def test_replace_me():
-    reader = io.StringIO("Line One\nLine Two\n")
+def test_full_using_pandas():    
     writer = io.StringIO()
-    weather.process_csv(reader, writer)
-    assert writer.getvalue() == "Saw 2 lines\n"
+    with open("./data/chicago_beach_weather.csv") as file:
+        weather.process_csv(file, writer)
+    writer.seek(0)
+    results = writer.readlines()[1:]
+
+    df = pd.read_csv("./data/chicago_beach_weather.csv")
+    df.insert(1, 'timestamp', df.apply(lambda x: datetime.strptime(x['Measurement Timestamp'], "%m/%d/%Y %I:%M:%S %p"), axis=1))
+    df.insert(1, 'date', df.apply(lambda x: x.timestamp.date(), axis=1))
+    
+    grouped_df =  df.groupby(['Station Name', 'date'], sort=False)
+
+    res_idx = 0
+    for key, _ in grouped_df:
+        print(f"Asserting {key}")
+
+        gdf = grouped_df.get_group(key)
+
+        res_splits = results[res_idx].split(',')
+               
+        sorted_df = gdf.sort_values(by=['timestamp'])
+        assert res_splits[0] == sorted_df['Station Name'].iloc[0]
+        assert res_splits[1] == datetime.strftime(sorted_df['date'].iloc[0], "%m/%d/%Y")
+
+        assert float(res_splits[2]) == sorted_df['Air Temperature'].min()
+        assert float(res_splits[3]) == sorted_df['Air Temperature'].max()
+        assert float(res_splits[4]) == sorted_df['Air Temperature'].iloc[0]
+        assert float(res_splits[5]) == sorted_df['Air Temperature'].iloc[-1]
+        
+        res_idx += 1
+
+        # if res_idx > 10:
+        #     break
+
+def test_weather_data_line_cls():
+    wdl = weather.WeatherDataLine("63rd Street Weather Station,12/31/2016 11:00:00 PM,-1.3,-2.8,73,0,0,39.3,0,264,2.2,3.2,992.3,5,354,11.8,12/31/2016 11:00 PM,63rdStreetWeatherStation201612312300")
+
+    assert wdl.station == "63rd Street Weather Station"
+    assert wdl.timestamp == datetime(2016, 12, 31, 23)
+    assert wdl.date == date(2016, 12, 31)
+    assert wdl.air_temp == -1.3
+
+    assert wdl.summary_key == "63rd Street Weather Station-2016-12-31"
